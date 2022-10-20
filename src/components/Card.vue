7fef9930c5cfd9112e17a14641312feefaaeb6f8
Александр <jawa_teapot@bk.ru>
2022-10-20 19:08:16 +0300

    fix2

---
+++ src/components/Card.vue
@@ -0,0 +1,123 @@
+<template>
+  <div
+    class="card"
+    :style="{ backgroundImage: 'url(' + card.preview_img_path + ')' }"
+  >
+    <div class="card__bottom">
+      <div>
+        <div class="card__headline">{{ card.series }}</div>
+        <div class="card__text">{{ card.title }}</div>
+      </div>
+      <div>
+        <div class="card__text">{{ card.cost }} {{ card.cost_currency }}</div>
+        <!--        <button class="card__btn">-->
+        <!--          Подробнее-->
+        <!--          <img-->
+        <!--            class="card__btn-img"-->
+        <!--            src="../assets/arrow-right.svg"-->
+        <!--            alt="img"-->
+        <!--          />-->
+        <!--        </button>-->
+      </div>
+    </div>
+    <div class="card__bottom-block">
+      <button class="card__btn">
+        Подробнее
+        <img class="card__btn-img" src="../assets/arrow-right.svg" alt="img" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  card: Object,
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 4px;
+  &:hover {
+    .card__bottom {
+      height: 100%;
+      padding-top: 165px;
+      padding-bottom: 70px;
+      background: linear-gradient(
+        0deg,
+        #4c5a79 0%,
+        rgba(76, 90, 121, 0.75) 100%
+      );
+    }
+    .card__btn {
+      bottom: 25px;
+      opacity: 1;
+    }
+  }
+  &__btn {
+    margin-top: 12px;
+    padding: 0;
+    border: none;
+    outline: none;
+    display: flex;
+    align-items: center;
+    background-color: transparent;
+    font-family: "Houschka Pro", sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+    color: #ffffff;
+    cursor: pointer;
+    position: absolute;
+    bottom: -10px;
+    opacity: 0;
+    transition: all 0.4s ease-in-out;
+    &-img {
+      margin-left: 10px;
+      width: 18px;
+      height: 8px;
+    }
+  }
+  &__bottom {
+    height: 225px;
+    padding: 56px 30px 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: linear-gradient(0deg, #4c5a79 0%, rgba(76, 90, 121, 0) 100%);
+    transition: all 0.6s;
+    &-block {
+      padding: 0 30px;
+      position: relative;
+      bottom: 0;
+    }
+  }
+  &__headline {
+    display: inline-block;
+    margin-bottom: 12px;
+    padding: 2px 10px;
+    background: #0066b3;
+    border-radius: 2px;
+    font-family: "Houschka Pro", sans-serif;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 19px;
+    color: #ffffff;
+  }
+  &__text {
+    font-family: "Houschka Pro", sans-serif;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 25px;
+    text-transform: uppercase;
+    color: #ffffff;
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,115 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="header">
+    <img class="header__img" src="../assets/logo.svg" alt="img" />
+    <div class="header__heading">Каталог курсов</div>
+    <div class="header__block">
+      <input
+        class="header__block-input"
+        type="text"
+        placeholder="Поиск по каталогу"
+      />
+      <img class="header__block-img" src="../assets/search.svg" alt="img" />
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="main">
+    <div class="main__top">
+      <div class="main__top-left">458 результатов</div>
+      <div class="main__top-right">
+        <div>Сортировать по:</div>
+        <div>Цене <img src="../assets/arrow-down.svg" alt="img" /></div>
+        <div>Алфавиту</div>
+      </div>
+    </div>
+    <div class="main__showcase">
+      <Card v-for="item in cards" :card="item"></Card>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script setup>
+import data from "../../public/data.json";
+console.log(data);
+import { ref, computed } from "vue";
+import Card from "./Card.vue";
+
+defineProps({});
+
+const cards = computed(() => {
+  return data;
+});
+
+const count = ref(0);
+</script>
+
+<style lang="scss" scoped>
+.header {
+  &__img {
+    margin: 30px 0;
+    width: 98px;
+    height: 36px;
+  }
+  &__heading {
+    font-family: "Futura PT", sans-serif;
+    margin-bottom: 30px;
+    font-weight: 600;
+    font-size: 42px;
+    line-height: 50px;
+    color: #4c5a79;
+  }
+  &__block {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 58px;
+    padding: 0 20px;
+    background-color: #f0f4fc;
+    border-radius: 4px;
+    &-input {
+      width: calc(100% - 40px);
+      outline: none;
+      border: none;
+      background-color: transparent;
+      font-family: "Houschka Pro", sans-serif;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 28px;
+      color: #4c5a79;
+      &::placeholder {
+        font-family: "Houschka Pro", sans-serif;
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 28px;
+        color: #4c5a79;
+      }
+    }
+    &-img {
+      width: 24px;
+      height: 24px;
+    }
+  }
+}
+.main {
+  margin-top: 30px;
+  &__top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &-left {
+      font-family: "Houschka Pro", sans-serif;
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 19px;
+      color: #4c5a79;
+    }
+    &-right {
+      display: flex;
+      align-items: center;
+    }
+  }
+  &__showcase {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 30px;
+  }
 }
 </style>
